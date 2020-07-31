e0b116b85dc5519232e63018bfa46fe0d444e2cd
Rocio01 <zrmartinezg@gmail.com>
2020-07-31 12:38:58 -0500

    Fix my_select method

--- prueba.rb
+++ prueba.rb
@@ -25,6 +25,20 @@ module Enumerable
       to_enum
     end
   end
-
   #   [2,10,12,15].my_each_with_index {|e, index| puts (e + e) if index.odd?}
+
+  def my_select
+    if block_given?
+      my_array = []
+      my_each do |w|
+        my_array.push(w) if yield w
+      end
+      my_array
+    else
+      to_enum
+    end
+  end
+  puts([1, 2, 3].my_select { |x| x != 2 })
+
+  # end of the module
 end
