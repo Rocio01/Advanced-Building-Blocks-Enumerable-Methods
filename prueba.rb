f569c60a232e1040af23186fa77974702d31685d
Rocio01 <zrmartinezg@gmail.com>
2020-07-31 12:15:05 -0500

    Fix my_each_with_index

--- prueba.rb
+++ prueba.rb
@@ -11,6 +11,20 @@ module Enumerable
       to_enum
     end
   end
-#   [2, 10, 12, 15].my_each { |e| puts e + e }
+  #   [2, 10, 12, 15].my_each { |e| puts e + e }
 
+  def my_each_with_index
+    if block_given?
+      w = 0
+      while w < size
+        yield(self[w], w)
+        w += 1
+      end
+      self
+    else
+      to_enum
+    end
+  end
+
+  #   [2,10,12,15].my_each_with_index {|e, index| puts (e + e) if index.odd?}
 end
