c3277ad52a7d715c45dbef0f7dd6df2c8d886286
jurgen1c <jurgen1c@gmail.com>
2020-09-02 15:33:35 -0600

    Create global test variables & tests for my_each and my_each_with_index

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -1,1 +1,19 @@
 require_relative '../enumerables.rb'
+
+describe Enumerable do
+    base_arr = [43, 5, 78, 10, 62]
+    let(:str_arr) { %w[beer joke night mic comedy]}
+    let(:range) { (4..16) }
+
+    describe '#my_each' do
+        it 'returns inputed array' do
+            expect(base_arr.my_each { |x| x}).to eql(base_arr)
+        end
+    end
+
+    describe '#my_each_with_index' do
+        it 'returns inputed array with indexes'
+        expect(base_arr.my_each_with_index { |x, y| }).to eql(base_arr)
+        end
+    end
+end
