823df7dd7e7d2d13c8ffd75feabd463b28e45c54
jurgen1c <jurgen1c@gmail.com>
2020-09-02 16:05:48 -0600

    Add test for my_select

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -1,6 +1,6 @@
 require_relative '../enumerables.rb'
 
-describe Enumerable do
+describe Enumerables do
     base_arr = [43, 5, 78, 10, 62]
     let(:str_arr) { %w[beer joke night mic comedy]}
     let(:range) { (4..16) }
@@ -16,4 +16,11 @@ describe Enumerable do
         expect(base_arr.my_each_with_index { |x, y| }).to eql(base_arr)
         end
     end
+
+    describe '#my_select' do
+        it 'returns elements in array matching condition' do
+            expect(range.my_select { |x| x % 4 == 0}).to eql([4, 8, 12, 16])
+        end
+    end
+
 end
